a37144d98cd7025fcd4115da80c863e85401f21e
AftoflBIG5 <AftoflBIG5@gmail.com>
2020-11-22 13:29:26 -0800

    Add cards :lipstick:

--- Front-End/src/components/complex/section/MiniCards/MiniCards.component.js
+++ Front-End/src/components/complex/section/MiniCards/MiniCards.component.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import {MiniCardsContainer} from './MiniCards.styles'
+import {MiniCardsContainer, FlexContainer, MiniCardContainer, MiniCard} from './MiniCards.styles'
 
 class Template extends Component {
     render() {
 
         return (
-            <MiniCardsContainer className="section-features">
-                <div className="row">
-                    <div className="col-1-of-4">
-                        <div className="feature-box u-center-text">
+            <MiniCardsContainer >
+                <FlexContainer>
+                    <MiniCardContainer>
+                        <MiniCard>
                             <i className="feature-box__icon icon-basic-world"></i>
                             <h3 className="heading-tertiary u-margin-bottom-small">
                                 Explore the world
@@ -17,11 +17,11 @@ class Template extends Component {
                                 shaja baja much loven disch very guten disch shaja baja much
                                 loven disch very guten disch
                   </p>
-                        </div>
-                    </div>
+                        </MiniCard>
+                    </MiniCardContainer>
 
-                    <div className="col-1-of-4">
-                        <div className="feature-box u-center-text">
+                    <MiniCardContainer>
+                        <MiniCard>
                             <i className="feature-box__icon icon-basic-map"></i>
                             <h3 className="heading-tertiary u-margin-bottom-small">
                                 Explore the world
@@ -30,11 +30,11 @@ class Template extends Component {
                                 shaja baja much loven disch very guten disch shaja baja much
                                 loven disch very guten disch
                   </p>
-                        </div>
-                    </div>
+                        </MiniCard>
+                    </MiniCardContainer>
 
-                    <div className="col-1-of-4">
-                        <div className="feature-box u-center-text">
+                    <MiniCardContainer>
+                        <MiniCard>
                             <i className="feature-box__icon icon-basic-heart"></i>
                             <h3 className="heading-tertiary u-margin-bottom-small">
                                 Explore the world
@@ -43,11 +43,11 @@ class Template extends Component {
                                 shaja baja much loven disch very guten disch shaja baja much
                                 loven disch very guten disch
                   </p>
-                        </div>
-                    </div>
+                        </MiniCard>
+                    </MiniCardContainer>
 
-                    <div className="col-1-of-4">
-                        <div className="feature-box u-center-text">
+                    <MiniCardContainer>
+                        <MiniCard>
                             <i className="feature-box__icon icon-basic-compass"></i>
                             <h3 className="heading-tertiary u-margin-bottom-small">
                                 Explore the world
@@ -56,9 +56,9 @@ class Template extends Component {
                                 shaja baja much loven disch very guten disch shaja baja much
                                 loven disch very guten disch
                   </p>
-                        </div>
-                    </div>
-                </div>
+                        </MiniCard>
+                    </MiniCardContainer>
+                </FlexContainer>
             </MiniCardsContainer>
         );
     }
--- Front-End/src/components/complex/section/MiniCards/MiniCards.styles.js
+++ Front-End/src/components/complex/section/MiniCards/MiniCards.styles.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import {respond} from '../../../../design/responsive';
+import shadows from '../../../../design/shadows.styles.jsx';
+
 
 
 //Code
@@ -6,4 +9,77 @@ export const MiniCardsContainer = styled.header`
   grid-column: full-start / full-end;
   padding: 10rem 1rem !important; 
   ${'' /* background-color: red; */}
+`;
+
+export const FlexContainer = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  ${respond.mobile.max`
+  ${'' /* flex-wrap: nowrap; */}
+  `}
+`;
+
+export const MiniCardContainer = styled.div`
+  ${'' /* margin: 2rem 20rem; */}
+  margin: 4rem 2rem;
+  ${'' /* flex: 0 0 200px; */}
+  
+  ${'' /* height: 100vw; */}
+  ${"" /* height: clamp(50px, 16.66666vw, 16.66666vw); */}
+`;
+export const MiniCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-items: flex-start;
+    background-color: rgba(255, 255, 255, 0.8);
+    overflow-wrap: normal;
+    word-wrap: normal;
+    word-break: keep-all;   
+    
+    width: clamp(50vw, 50vw, 150px);
+    
+    padding: 3rem 1rem;
+    text-align: center;
+    border-radius: 15px;
+    box-shadow: 0 1.5rem 4rem rgba(0, 0, 0, 0.22);
+    ${shadows.mixins.neumorphic.original}
+    transition: transform 0.4s;
+  }
+  h3 {
+    font-size: clamp(10px, 3vw, 3vw);
+  }
+  p {
+     ${'' /* text-align: justify;
+    text-justify: inter-word;  */}
+    font-size: clamp(10px, 3vw, 30px);
+      
+  }
+  ${
+    "" /* @media only screen and (max-width: 56.25em) {
+    & {
+      padding: 2rem;
+    }
+  } */
+  }
+  &:hover {
+    transform: translateY(-1rem) scale(1.05);
+    box-shadow: 0 1.5rem 4rem rgba(0, 0, 0, 0.52);
+  }
+  &__icon {
+    font-size: 4rem;
+    margin-bottom: 0.5rem;
+    display: inline-block;
+    background-image: linear-gradient(to right, #6bb2db, #094e7c);
+    -webkit-background-clip: text;
+    color: transparent;
+  }
+  ${
+    "" /* @media only screen and (max-width: 56.25em) {
+    &__icon {
+      margin-bottom: 0;
+    } */
+  }
 `;
